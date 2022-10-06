422ea16b7a48348d993a6e22238bae99a9e99250
Jaypen-git <54304688+Jaypen-git@users.noreply.github.com>
2022-10-05 21:59:00 -0400

    styles slider section

--- src/Slider.js
+++ src/Slider.js
@@ -4,7 +4,9 @@ const Slider = () => {
     return (
         <div className="slider">
             <SliderCard />
-            <img src={require("./img/banner.png")} alt="banner1" className="slider-img" />
+            <div className="img-container">
+                <img src={require("./img/banner.png")} alt="banner1" className="slider-img" />
+            </div>
         </div>    
     );
 }
