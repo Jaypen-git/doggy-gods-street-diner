bbb350865f11aa444f733da30dcb7175068c4204
Jaypen-git <54304688+Jaypen-git@users.noreply.github.com>
2022-10-06 14:27:50 -0400

    sets basic structure of signup section

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import About from "./About";
 import Hero from "./Hero";
+import SignUp from "./SignUp";
 
 const Main = () => {
     return (
         <main>
             <Hero />
             <About />
+            <SignUp />
         </main>
     );
 }
---
+++ src/SignUp.js
@@ -0,0 +1,12 @@
+const SignUp = () => {
+    return (
+        <section className="signup">
+            <h1>Newsletter</h1>
+            <h3 className="signup-small">Sign up for our newsletter & get exclusive offers and invites!</h3>
+            <input type="text"/>
+            <button className="signup-small">Sign Up!</button>
+        </section>
+    );
+}
+ 
+export default SignUp;
